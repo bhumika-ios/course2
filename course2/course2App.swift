44319235643349aefa327fafaf4286e7c70a4d22
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-08-04 11:55:25 +0530

    Initial Commit

---
+++ course2/course2App.swift
@@ -0,0 +1,17 @@
+//
+//  course2App.swift
+//  course2
+//
+//  Created by Bhumika Patel on 04/08/23.
+//
+
+import SwiftUI
+
+@main
+struct course2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
